10bdacad9feaf3a4293d5f43bfda882c8b1346a6
hamiltonspjr <hamilton-souza@outlook.com.br>
2023-05-20 18:34:14 -0300

    feat: added routes in the application

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,6 +1,11 @@
 import { StatusBar } from 'expo-status-bar'
 import { ImageBackground, View, Text, TouchableOpacity } from 'react-native'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import * as SecureStore from 'expo-secure-store'
+import { styled } from 'nativewind'
+import { useEffect } from 'react'
+import { api } from '../src/lib/api'
+import { useRouter } from 'expo-router'
 
 import {
   useFonts,
@@ -10,11 +15,9 @@ import {
 
 import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from './src/assets/bg-blur.png'
-import Stripes from './src/assets/stripes.svg'
-import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
-import { styled } from 'nativewind'
-import { useEffect } from 'react'
+import blurBg from '../src/assets/bg-blur.png'
+import Stripes from '../src/assets/stripes.svg'
+import NLWLogo from '../src/assets/nlw-spacetime-logo.svg'
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -26,13 +29,15 @@ const discovery = {
 const StyledStripes = styled(Stripes)
 
 export default function App() {
+  const router = useRouter()
+
   const [hasLoadedFonts] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
     BaiJamjuree_700Bold,
   })
 
-  const [request, response, signInWithGithub] = useAuthRequest(
+  const [response, signInWithGithub] = useAuthRequest(
     {
       clientId: '6dfad506aeb99a79424d',
       scopes: ['identity'],
@@ -43,10 +48,20 @@ export default function App() {
     discovery,
   )
 
+  async function handleGithubOAuthCode(code: string) {
+    const response = await api.post('/register', {
+      code,
+    })
+    const { token } = response.data
+    await SecureStore.setItemAsync('token', token)
+
+    router.push('/memories')
+  }
+
   useEffect(() => {
-    console.log(response)
     if (response?.type === 'success') {
       const { code } = response.params
+      handleGithubOAuthCode(code)
     }
   }, [response])
 
